1d8da39db3ea2bdfdb1986f8cd88f2ce9b764efe
Patchara Pattanakimhun <s6070091@kmitl.ac.th>
2016-08-25 17:28:51 +0700

    display confirmOrder

--- MyApplication/app/src/main/java/com/example/justwyne/windowshopping/CartActivity.java
+++ MyApplication/app/src/main/java/com/example/justwyne/windowshopping/CartActivity.java
@@ -22,7 +22,11 @@ public class CartActivity extends AppCompatActivity {
     private Button btnCheckout;
     private LinearLayout linearLayoutOrder;
     private LinearLayout.LayoutParams paramsOrder;
+    private LinearLayout.LayoutParams paramsBtnRemove;
     private TextView tvProductOrder;
+    private TextView tvSubTotal;
+    private TextView tvTotal;
+    private Button btnRemove;
     private Cart cart;
 
 
@@ -38,8 +42,11 @@ public class CartActivity extends AppCompatActivity {
     }
 
     private void initInstances(){
+        tvSubTotal = (TextView) findViewById(R.id.tvSubTotal);
+        tvTotal = (TextView) findViewById(R.id.tvTotal);
         linearLayoutOrder = (LinearLayout) findViewById(R.id.cartLayout);
-        paramsOrder = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        paramsOrder = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        paramsBtnRemove = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
         btnCheckout = (Button) findViewById(R.id.btnCheckout);
         btnCheckout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,15 +66,46 @@ public class CartActivity extends AppCompatActivity {
 
             LinearLayout orderLayout = new LinearLayout(this);
             orderLayout.setOrientation(LinearLayout.HORIZONTAL);
+            LinearLayout btnRemoveLayout = new LinearLayout(this);
+            btnRemoveLayout.setOrientation(LinearLayout.HORIZONTAL);
 
             tvProductOrder = new TextView(this);
-            tvProductOrder.setText(String.format("Product ID: %s\nProduct: %s\nColor: %s\nSize: %s\nQuantity %s\n", product.getId(), product.getName(), color.getName(), size, quantity));
-            tvProductOrder.setLayoutParams(paramsOrder);
+            tvProductOrder.setText(String.format("Product ID: %s\nProduct: %s\nColor: %s\nSize: %s\nQuantity %s", product.getId(), product.getName(), color.getName(), size, quantity));
+            tvProductOrder.setTextColor(android.graphics.Color.parseColor("#000000"));
 
+            for(int index=0; index < cart.size(); index++)
+            {
+
+                btnRemove = new Button(this);
+                btnRemove.setId(index+1);
+                btnRemove.setText("remove");
+                paramsBtnRemove.setMargins(0, 0, 20, 0);
+                btnRemove.setLayoutParams(paramsBtnRemove);
+
+
+
+            }
+
+            View view = new View(this);
+            view.setLayoutParams(new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    5
+            ));
+            view.setBackgroundColor(android.graphics.Color.parseColor("#B3B3B3"));
+
+
+
+            tvProductOrder.setLayoutParams(paramsOrder);
+            btnRemoveLayout.addView(btnRemove);
             orderLayout.addView(tvProductOrder);
             linearLayoutOrder.addView(orderLayout);
+            linearLayoutOrder.addView(btnRemoveLayout);
+            linearLayoutOrder.addView(view);
         }
+        tvSubTotal.setText(String.format("$ %.2f", cart.getTotal()));
+        tvTotal.setText(String.format("$ %.2f", cart.getTotal()));
     }
+
 
     private void sendIntent(){
         Intent intent;
--- MyApplication/app/src/main/java/com/example/justwyne/windowshopping/ConfirmOrderActivity.java
+++ MyApplication/app/src/main/java/com/example/justwyne/windowshopping/ConfirmOrderActivity.java
@@ -5,19 +5,42 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.justwyne.windowshopping.models.Cart;
+import com.example.justwyne.windowshopping.models.Color;
+import com.example.justwyne.windowshopping.models.Product;
+import com.example.justwyne.windowshopping.models.ProductOrder;
 
 public class ConfirmOrderActivity extends AppCompatActivity {
     private Button btnOrder;
+    private LinearLayout linearLayoutOrder;
+    private LinearLayout.LayoutParams paramsOrder;
+    private LinearLayout.LayoutParams paramsBtnRemove;
+    private TextView tvProductOrder;
+    private TextView tvSubTotal;
+    private TextView tvTotal;
+    private Button btnRemove;
+    private Cart cart;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirm_order);
 
+        cart = Cart.getInstance();
         initInstances();
+        createProductOrder();
     }
 
     private void initInstances(){
+        tvSubTotal = (TextView) findViewById(R.id.tvSubTotal);
+        tvTotal = (TextView) findViewById(R.id.tvTotal);
+        linearLayoutOrder = (LinearLayout) findViewById(R.id.confirmOrderLayout);
+        paramsOrder = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        paramsBtnRemove = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
         btnOrder = (Button) findViewById(R.id.btnOrder);
         btnOrder.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,6 +48,55 @@ public class ConfirmOrderActivity extends AppCompatActivity {
                 sendIntent();
             }
         });
+    }
+
+    private void createProductOrder(){
+        for (ProductOrder productOrder : cart.getProductList()) {
+
+            Product product = productOrder.getProduct();
+            Color color = productOrder.getColor();
+            String size = productOrder.getSize();
+            int quantity = productOrder.getQuantity();
+
+            LinearLayout orderLayout = new LinearLayout(this);
+            orderLayout.setOrientation(LinearLayout.HORIZONTAL);
+            LinearLayout btnRemoveLayout = new LinearLayout(this);
+            btnRemoveLayout.setOrientation(LinearLayout.HORIZONTAL);
+
+            tvProductOrder = new TextView(this);
+            tvProductOrder.setText(String.format("Product ID: %s\nProduct: %s\nColor: %s\nSize: %s\nQuantity %s", product.getId(), product.getName(), color.getName(), size, quantity));
+
+            for(int index=0; index < cart.size(); index++)
+            {
+
+                btnRemove = new Button(this);
+                btnRemove.setId(index+1);
+                btnRemove.setText("remove");
+                paramsBtnRemove.setMargins(0, 0, 20, 0);
+                btnRemove.setLayoutParams(paramsBtnRemove);
+
+
+
+            }
+
+            View view = new View(this);
+            view.setLayoutParams(new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    5
+            ));
+            view.setBackgroundColor(android.graphics.Color.parseColor("#B3B3B3"));
+
+
+
+            tvProductOrder.setLayoutParams(paramsOrder);
+            btnRemoveLayout.addView(btnRemove);
+            orderLayout.addView(tvProductOrder);
+            linearLayoutOrder.addView(orderLayout);
+            linearLayoutOrder.addView(btnRemoveLayout);
+            linearLayoutOrder.addView(view);
+        }
+        tvSubTotal.setText(String.format("$ %.2f", cart.getTotal()));
+        tvTotal.setText(String.format("$ %.2f", cart.getTotal()));
     }
 
     private void sendIntent(){
