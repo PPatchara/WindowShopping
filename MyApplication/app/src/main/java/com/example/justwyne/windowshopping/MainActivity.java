de5d93db25f695658e7e8ab0c68e659722144e32
Patchara Pattanakimhun <s6070091@kmitl.ac.th>
2016-08-26 00:23:28 +0700

    forward swipe & backward swipe

--- MyApplication/app/src/main/java/com/example/justwyne/windowshopping/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/justwyne/windowshopping/MainActivity.java
@@ -11,10 +11,15 @@ import java.nio.channels.NotYetConnectedException;
 
 public class MainActivity extends BaseActivity {
     private static final int REQ_CODE = 001;
+    public static final int MIN_SWIPE_TIME = 160;
 
     private int oldX = 0;
     private int oldY = 0;
     private int touchState = 0;
+
+    float initialX, initialY;
+    long startTime = 0;
+    String TAG = "onTouchEvent";
 
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,63 +31,102 @@ public class MainActivity extends BaseActivity {
     {
         int current_x=(int) motionEvent.getX();
         int current_y=(int) motionEvent.getY();
+        int action = motionEvent.getActionMasked();
 
-        switch (motionEvent.getAction())
+        switch (action)
         {
             case MotionEvent.ACTION_DOWN:
                 Log.d("DEBUG", "On touch (down)" + String.valueOf(current_x) + ", " + String.valueOf(current_y));
-                oldX = current_x;
-                oldY = current_y;
 
+                if (startTime == 0){
+                    startTime = System.currentTimeMillis();
+                }
+
+                initialX = current_x;
+                initialY = current_y;
                 touchState = 1;
 
                 break;
             case MotionEvent.ACTION_UP:
-                Log.d("DEBUG", "On touch (up)" + String.valueOf(current_x) + ", " + String.valueOf(current_y));
-                oldX = current_x;
-                oldY = current_y;
+                float finalX = motionEvent.getX();
+                long endTime = (System.currentTimeMillis() - startTime);
+                Log.d("DEBUG", "On touch (up)" + String.valueOf(current_x) + ", " + String.valueOf(current_y) + " : " + endTime);
+
+                if (endTime < MIN_SWIPE_TIME && touchState == 0) {
+                    if (initialX < finalX) {
+                        Log.d("On touch", "Left to Right swipe performed: Forward");
+                        sendSwipe(initialX,finalX);
+                    }
+                    else if (initialX > finalX) {
+                        Log.d("On touch", "Right to Left swipe performed: Backward");
+                        sendSwipe(initialX,finalX);
+                    }
+                }
+                else if (oldX == current_x && oldY == current_y && touchState == 1) {
+                    sendMouseTapped();
+                }
+                startTime = 0;
 
                 break;
             case MotionEvent.ACTION_MOVE:
-                Log.d("DEBUG", "On touch (move)" + String.valueOf(current_x) + ", " + String.valueOf(current_y));
+                if (System.currentTimeMillis() - startTime >= MIN_SWIPE_TIME) {
+                    Log.d("DEBUG", "On touch (move)" + String.valueOf(current_x) + ", " + String.valueOf(current_y));
 
+                    int deltaX = current_x - oldX;
+                    int deltaY = current_y - oldY;
+
+                    sendMouseMove(deltaX, deltaY);
+                }
                 touchState = 0;
-
                 break;
         }
-        if( webSocket != null && motionEvent.getAction() != MotionEvent.ACTION_DOWN && motionEvent.getAction() != MotionEvent.ACTION_UP) {
+//        if( webSocket != null && motionEvent.getAction() != MotionEvent.ACTION_DOWN && motionEvent.getAction() != MotionEvent.ACTION_UP) {
 
-            int deltaX = current_x - oldX;
-            int deltaY = current_y - oldY;
+//            int deltaX = current_x - oldX;
+//            int deltaY = current_y - oldY;
+//
+//            oldX = current_x;
+//            oldY = current_y;
 
-            oldX = current_x;
-            oldY = current_y;
+//        } else
+//        if(webSocket != null && motionEvent.getAction() == MotionEvent.ACTION_UP && touchState == 1) {
+//            try {
+//                String json_data = String.format("{\"action\": \"tap\", \"pos_x\": %d, \"pos_y\":%d}", current_x, current_y);
+//                webSocket.send(json_data);
+//            } catch(NotYetConnectedException e) {
+//                Toast.makeText(this, "Disconnected!", Toast.LENGTH_LONG).show();
+//                e.printStackTrace();
+//
+//            }
+//            Log.d("DEBUG", "Tapped");
+//        }
 
-
-            Log.d("DEBUG", "On touch (delta)" + String.valueOf(deltaX) + ", " + String.valueOf(deltaY));
-
-            try {
-                String json_data = String.format("{\"action\": \"pos\", \"delta_x\": %d, \"delta_y\":%d}", deltaX, deltaY);
-                webSocket.send(json_data);
-            } catch(NotYetConnectedException e) {
-                Toast.makeText(this, "Disconnected!", Toast.LENGTH_LONG).show();
-                e.printStackTrace();
-
-            }
-        }else if(webSocket != null && motionEvent.getAction() == MotionEvent.ACTION_UP && touchState == 1){
-
-            try {
-                String json_data = String.format("{\"action\": \"tap\", \"pos_x\": %d, \"pos_y\":%d}", current_x, current_y);
-                webSocket.send(json_data);
-            } catch(NotYetConnectedException e) {
-                Toast.makeText(this, "Disconnected!", Toast.LENGTH_LONG).show();
-                e.printStackTrace();
-
-            }
-            Log.d("DEBUG", "Tapped");
-        }
+        oldX = current_x;
+        oldY = current_y;
         return true;
     }
+
+    private void sendMouseMove(int deltaX, int deltaY) {
+        String json_data = String.format("{\"action\": \"pos\", \"delta_x\": %d, \"delta_y\":%d}", deltaX, deltaY);
+        webSocket.send(json_data);
+    }
+
+    private void sendMouseTapped() {
+        Log.d("On touch (tap)", "Tapped");
+        String json_data = String.format("{\"action\": \"tap\"}");
+        webSocket.send(json_data);
+    }
+
+    private void sendSwipe(float initialX,float finalX){
+        if (initialX < finalX){
+            String json_data = String.format("{\"action\": \"forward swipe\"}");
+            webSocket.send(json_data);
+        }else if (initialX > finalX){
+            String json_data = String.format("{\"action\": \"backward swipe\"}");
+            webSocket.send(json_data);
+        }
+    }
+
 
     @Override
     protected void onPause() {
